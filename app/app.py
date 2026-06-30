3b01a2e930557e9637530454445192de6da27f93
Nick Stach <tristach@yahoo.com>
2026-07-01 06:11:33 +0900

    changes to html

--- app/app.py
+++ app/app.py
@@ -2,14 +2,13 @@ import os
 from dotenv import load_dotenv
 from azure.identity import ClientSecretCredential
 from azure.monitor.query import LogsQueryClient
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 
 load_dotenv()
 
 app = Flask(__name__)
 
 tenant_id = os.getenv("AZURE_TENANT_ID")
-
 client_id = os.getenv("AZURE_CLIENT_ID")
 client_secret = os.getenv("AZURE_CLIENT_SECRET")
 workspace_id = os.getenv("LOG_ANALYTICS_WORKSPACE_ID")
@@ -26,36 +25,49 @@ query = """
 SecurityAlert
 | take 5
 """
-@app.route("/alerts")
-def get_alerts():
+
+
+def get_alert_data():
     response = client.query_workspace(
         workspace_id,
         query,
         timespan=None,
-)
+    )
+
+    alerts = []
 
     if response.tables:
         table = response.tables[0]
 
+        for row in table.rows:
+            alert = {
+                "alert_name": row[3],
+                "severity": row[4],
+                "time": str(row[1]),
+                "provider": row[6],
+                "status": row[29],
+                "tactics": row[31],
+                "techniques": row[32],
+                "entity": row[30],
+                "description": row[5],
+            }
 
-    alerts = []
+            alerts.append(alert)
 
-    for row in table.rows:
-        alert = {
-            "alert_name": row[3],
-            "severity": row[4],
-            "time": str(row[1]),
-            "provider": row[6],
-            "status": row[29],
-            "tactics": row[31],
-            "techniques": row[32],
-            "entity": row[30],
-            "description": row[5],
-        }
+    return alerts
 
-        alerts.append(alert)
 
+@app.route("/")
+def dashboard():
+    alerts = get_alert_data()
+    return render_template("index.html", alerts=alerts)
+
+
+@app.route("/alerts")
+def get_alerts():
+    alerts = get_alert_data()
     return jsonify(alerts)
 
+
 if __name__ == "__main__":
-        app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
